7c65a1ba1ec7edb99b9d74c83eaf51a4afe1efea
umaranis <mail@umaranis.com>
2023-04-26 21:51:52 +1000

    first commit

---
+++ src/PlaintextComposer.svelte
@@ -0,0 +1,34 @@
+<script lang='ts'>
+    import {
+        Composer,
+        ContentEditable,
+        ActionBar,
+        PlainTextPlugin,
+        HistoryPlugin
+    } from 'svelte-lexical';
+    //import PlaygroundEditorTheme from './themes/PlaygroundEditorTheme';
+
+    const initialConfig = {
+        //theme: PlaygroundEditorTheme,
+        nodes: [],
+        onError: (error: Error) => {
+            throw error;
+        }
+    };
+</script>
+
+<Composer {initialConfig}>
+    <div class="editor-shell">
+        <div class="editor-container">
+            <div class="editor-scroller">
+                <div class="editor">
+                    <ContentEditable />
+                </div>
+            </div>
+            <PlainTextPlugin />
+            <HistoryPlugin />
+
+            <ActionBar />
+        </div>
+    </div>
+</Composer>
